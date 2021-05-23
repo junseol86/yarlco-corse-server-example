f950f3a20f183ae636b56e2f7534337e48d8e499
Channel Yalco <yalco@kakao.com>
2021-05-24 01:16:34 +0900

    add -A

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { ClassModule } from './modules/class/class.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ClassModule);
+  app.enableCors({
+    origin: 'localhost',
+    methods: 'GET,POST,PUT,DELETE'
+  })
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
