9363efc4c09d925264d4f223f0bdcf9e6e7d5915
Channel Yalco <yalco@kakao.com>
2021-05-23 23:58:24 +0900

    add blank get

--- src/modules/class/class.controller.ts
+++ src/modules/class/class.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { ClassService } from './class.service';
 
-@Controller('class')
+@Controller()
 export class ClassController {
   constructor(private readonly classService: ClassService) {}
 
   @Get()
+  getHello(): string {
+    return 'HELLO!'
+  }
+
+  @Get('/class')
   getClasses(): string[] {
     return this.classService.getClasses();
   }
