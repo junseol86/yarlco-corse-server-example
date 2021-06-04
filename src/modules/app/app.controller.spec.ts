f5f6fc9fe83cfaf120acdc58c29e22c5fd8ad3b6
Channel Yalco <yalco@kakao.com>
2021-06-05 00:29:14 +0900

    ADD travis code

--- src/modules/app/app.controller.spec.ts
+++ src/modules/app/app.controller.spec.ts
@@ -19,4 +19,10 @@ describe('AppController', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
 });
